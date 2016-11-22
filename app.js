9503a48e2fd184006c3af96d470748167b93c25c
nutnaik <nutnaik@cisco.com>
2016-11-22 19:50:49 +0530

    Assignment 5.3

---
+++ app.js
@@ -0,0 +1,18 @@
+
+var Employee = {
+	name : "Nutan",
+	age : 24,
+	salary : 10000,
+	address : {
+		city : "Bangalore",
+		State : "Karnataka",
+		pincode : "560043"
+	}
+}
+
+console.log("Name : " +Employee.name);
+console.log("age : " +Employee.age);
+console.log("salary : " +Employee.salary);
+console.log("city : " +Employee.address.city);
+console.log("state : " +Employee.address.State);
+console.log("pincode : " +Employee.address.pincode);
